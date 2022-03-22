5144986fe2e85d2422d6a7c3beb81c21b73bdd33
MORISHIMA Masahito <morishima@pnop.co.jp>
2022-03-22 11:57:11 +0900

    Update libraries version and Rewritten to not use obsolete classes

--- web-application/src/main/java/com/example/web/sample/config/CustomWebConfigAdapter.java
+++ web-application/src/main/java/com/example/web/sample/config/CustomWebConfigAdapter.java
@@ -1,12 +1,17 @@
 package com.example.web.sample.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CustomWebConfigAdapter implements WebMvcConfigurer { 
+public class CustomWebConfigAdapter implements WebMvcConfigurer {
+
+    @Autowired
+    private CustomWebHandlerInterceptor _handler;
+
     @Override
     public void addInterceptors(final org.springframework.web.servlet.config.annotation.InterceptorRegistry registry) {
-        registry.addInterceptor(new CustomHeaderInterceptorAdapter());
+        registry.addInterceptor(_handler);
     }
 }
---
+++ web-application/src/main/java/com/example/web/sample/config/CustomWebHandlerInterceptor.java
@@ -0,0 +1,50 @@
+package com.example.web.sample.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.microsoft.applicationinsights.web.internal.RequestTelemetryContext;
+import com.microsoft.applicationinsights.web.internal.ThreadContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class CustomWebHandlerInterceptor implements HandlerInterceptor {
+
+    private static String HEADER = "X-MyHeader";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+        throws Exception {
+
+        try {
+
+            // Get current request telemetry in current context and set some custom
+            // properties.
+
+            RequestTelemetryContext context = ThreadContext.getRequestTelemetryContext();
+
+            if (context == null) {
+                return true;
+            }
+
+            // Set my custom property
+            context.getHttpRequestTelemetry().getProperties().put("MyCustomProperty1", "In interceptor");
+
+            String value = request.getHeader(HEADER);
+
+            if (value == null) {
+                return true;
+            }
+
+            // Set some request header
+            context.getHttpRequestTelemetry().getProperties().put(HEADER, value);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+}
